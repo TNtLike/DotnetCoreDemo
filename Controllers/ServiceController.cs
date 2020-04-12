eefd7a2ed8b251d520b6c8989db5a32a42db9d61
TNtLike <qiulangcheng@gmail.com>
2020-04-12 16:33:39 +0800

    fix

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -6,51 +6,52 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyWebApi.Models;
 using MyWebApi.Services;
-using System.Text.Json;
 
 namespace MyWebApi
 {
-    public class CarController : ControllerBase
+    public class ServiceController : ControllerBase
     {
         private readonly CarService _carservice;
         private readonly QRCodeService _qrservice;
-        public CarController(CarService carserver, QRCodeService qrservice)
+        public ServiceController(CarService carserver, QRCodeService qrservice)
         {
             _carservice = carserver;
             _qrservice = qrservice;
         }
 
         [HttpGet]
-        public IActionResult Cars()
+        public IActionResult Cars(string id)
         {
-            List<Car> rtnmsg = _carservice.GetTs();
-            if (rtnmsg.Count <= 0)
+            if (string.IsNullOrEmpty(id))
             {
-                return NotFound();
+                List<Car> listmsg = _carservice.GetTs();
+                if (listmsg.Count <= 0)
+                {
+                    return NotFound();
+                }
+                return Ok(listmsg);
             }
-            return Ok(rtnmsg);
-        }
+            else
+            {
+                Car rtnmsg = _carservice.GetT(id);
+                if (rtnmsg == null)
+                {
+                    return NotFound();
+                }
+                return Ok(rtnmsg);
+            }
 
-        [HttpGet]
-        public IActionResult Car(string id)
-        {
-            Car rtnmsg = _carservice.GetT(id);
-            if (rtnmsg == null)
-            {
-                return NotFound();
-            }
-            return Ok(rtnmsg);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Car([FromBody] Car car)
+        public async Task<IActionResult> Cars([FromBody] Car car)
         {
             var rtnmsg = await _carservice.CreateAsync(car);
             return Ok(rtnmsg);
         }
 
         [HttpPut]
-        public async Task<IActionResult> Car(string id, [FromBody] Car car)
+        public async Task<IActionResult> Cars(string id, [FromBody] Car car)
         {
             var rtnmsg = await _carservice.UpdateAsync(id, car);
             return Ok(rtnmsg);
